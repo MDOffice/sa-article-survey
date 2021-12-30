5a40ed10ef078823679b0cf53ffafbcfbd02b078
kelatev <kelatev@gmail.com>
2021-12-30 12:54:00 +0200

    fix:

--- src/i18n/uk.js
+++ src/i18n/uk.js
@@ -1,12 +1,10 @@
-(function ($) {
-    $.extend($.SAArticleSurvey.defaults, {
-        titleUseful: 'Была ли эта статья полезна?',
-        titleThx: 'Благодарим за отзыв.',
-        titleSor: 'Приносим извинения.',
-        titleImprove: 'Как можно улучшить эту статью?',
-        titleNeedText: 'Укажите текст замечания',
-        btnUseful: 'Да',
-        btnNoUseful: 'Нет',
-        btnSend: 'Отправить'
-    });
-}(jQuery));
+export default UK = {
+    titleUseful: 'Была ли эта статья полезна?',
+    titleThx: 'Благодарим за отзыв.',
+    titleSor: 'Приносим извинения.',
+    titleImprove: 'Как можно улучшить эту статью?',
+    titleNeedText: 'Укажите текст замечания',
+    btnUseful: 'Да',
+    btnNoUseful: 'Нет',
+    btnSend: 'Отправить'
+};
